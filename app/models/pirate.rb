b3cba4f718ed524eaff174ab941f7f2dce4e4707
Flatiron School <github email address>
2016-08-20 08:47:51 -0400

    done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+
+  PIRATES = []
+
+  attr_accessor :name, :weight, :height
+
+  def initialize(hash)
+    @name = hash[:name]
+    @weight = hash[:weight]
+    @height = hash[:height]
+
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
